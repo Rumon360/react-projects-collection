01a9605528e0d19015e780b8c66f4857516e9020
Rumon360 <hmk.rumon@gmail.com>
2025-11-10 00:42:23 +0600

    chore: netlify fix 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), netlifyPlugin()],
+  plugins: [react(), tailwindcss()],
 });
